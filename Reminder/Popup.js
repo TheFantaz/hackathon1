64021b3a566443eace7e6c4df2760d87bb665da8
MovinIn <87917333+MovinIn@users.noreply.github.com>
2021-07-24 19:40:47 -0700

    Update Popup.js

--- Reminder/Popup.js
+++ Reminder/Popup.js
@@ -1,4 +1,3 @@
-// JavaScript Document
 function myFunction() {
 
     var the_text = document.getElementById("RText").value;
@@ -6,3 +5,10 @@ function myFunction() {
 
     setTimeout(function(){ alert(the_text); }, the_time*1000);
 }
+
+document.getElementById("RButton").addEventListener("click", function() {
+    var the_text = document.getElementById("RText").value;
+    var the_time = document.getElementById("RTIME").value;
+
+    setTimeout(function(){ alert(the_text); }, the_time*1000);
+});
